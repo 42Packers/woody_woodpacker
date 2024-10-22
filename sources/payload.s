219e7128eccd29ff5ef057cd3abc0e1e5fe60e64
noctuelles <paul@plouvel.dev>
2024-10-22 22:18:32 +0200

    feat: writing payload

--- sources/payload.s
+++ sources/payload.s
@@ -1,23 +1,82 @@
-%define WRITE_SYSNO 1
-%define STDOUT_FILENO 1
+bits 64
 
-payload:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 0x10
+%define SYS_WRITE 1
+%define SYS_EXIT 60
+%define SYS_LSEEK 8
+%define SYS_SENDFILE 40
+%define SYS_MEMFD_CREATE 319
+%define EXECVE 59
+%define STDOUT 1
+%define SYS_OPEN 2
 
-    mov DWORD [rbp - 4],  0x00000A2E ;    .
-    mov DWORD [rbp - 8],  0x2E2E2E59 ; ...Y
-    mov DWORD [rbp - 12], 0x444F4F57 ; DOOW
-    mov DWORD [rbp - 16], 0x2E2E2E2E ; ....
+%define MFD_CLOEXEC 0x0001
 
-    mov rax, WRITE_SYSNO
-    mov rdi, STDOUT_FILENO
-    lea rsi, [rbp - 16]
-    mov rdx, 14
+section .rodata
+    message db "....WOODY....", 0x0A, 0
+    message_len equ $ - message
+    self_path db '/proc/self/exe', 0
+    woody db 'woody', 0
+    file_size dq 9040
+
+section .text
+    global _start
+
+_start:
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    lea rsi, [rel message]
+    mov rdx, message_len
     syscall
 
-    pop rbp
+    ; Open self
+    mov rax, SYS_OPEN
+    mov rdi, self_path
+    xor rsi, rsi
+    syscall
 
-    mov rax, 0x0000000000000000 ; to be patched
-    jmp rax
+    test rax, rax
+    mov rdi, 1
+    js _exit
+
+    mov r11, rax
+
+    ; Seek to file size
+    mov rax, SYS_LSEEK
+    mov rdi, r11
+    mov rsi, file_size
+    mov rdx, 0
+    syscall
+
+    ; Open memfd
+    mov rax, SYS_MEMFD_CREATE
+    lea rdi, [rel woody]
+    mov rsi, MFD_CLOEXEC
+    syscall
+
+    test rax, rax
+    mov rdi, 1
+    js _exit
+
+    mov r12, rax
+
+    ; Transfer file to memfd
+    mov rax, SYS_SENDFILE
+    mov rdi, r12
+    mov rsi, r11
+    mov rdx, 0x00
+    mov r10, 30
+    syscall
+
+    ; Execve the memory file
+    mov r
+
+    test rax, rax
+    mov rdi, 1
+    js _exit
+
+    mov r12, rax
+
+    xor rdi, rdi
+_exit:
+    mov rax, SYS_EXIT
+    syscall
