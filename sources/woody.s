71d5403867363eb58e1fd966cd08cf446bd3b1e6
noctuelles <paul@plouvel.dev>
2024-10-26 15:33:14 +0200

    feat: working packing

---
+++ sources/woody.s
@@ -0,0 +1,97 @@
+bits 64
+
+; Syscall numbers
+%define SYS_WRITE 1
+%define SYS_EXIT 60
+%define SYS_LSEEK 8
+%define SYS_SENDFILE 40
+%define SYS_MEMFD_CREATE 319
+%define SYS_EXECVEAT 322
+%define SYS_OPEN 2
+
+; Flags for memfd_create
+%define MFD_CLOEXEC 0001h
+
+; Flags for execveat
+%define AT_EMPTY_PATH 1000h
+
+; Misc
+%define STDOUT 1
+
+section .rodata
+    message db "....WOODY....", 0Ah, 0
+    message_len equ $ - message
+    self_path db '/proc/self/exe', 0
+    woody db 'woody', 0
+    empty_str db 0
+
+    woody_size dq 9744 ; This is the size of the packer
+    payload_size dq 1385416 ; This is the size of the payload it will unpack
+
+section .text
+    global _start
+
+_start:
+    mov rax, SYS_WRITE
+    mov rdi, STDOUT
+    lea rsi, [rel message]
+    mov rdx, message_len
+    syscall
+
+    ; Open self file
+    mov rax, SYS_OPEN
+    mov rdi, self_path
+    xor rsi, rsi
+    syscall
+
+    test rax, rax
+    js _exit
+
+    mov r12, rax
+
+    ; Seek to file size
+    mov rax, SYS_LSEEK
+    mov rdi, r12
+    xor rsi, rsi
+    mov rsi, [rel woody_size]
+    mov rdx, 0
+    syscall
+
+    ; Open memfd
+    mov rax, SYS_MEMFD_CREATE
+    lea rdi, [rel woody]
+    mov rsi, MFD_CLOEXEC
+    syscall
+
+    test rax, rax
+    js _exit
+
+    mov r13, rax
+
+    ; Transfer file to the memfd
+    mov rax, SYS_SENDFILE
+    mov rdi, r13
+    mov rsi, r12
+    mov rdx, 0h
+    mov r10, [rel payload_size]
+    syscall
+
+    test rax, rax
+    js _exit
+
+    ; Execve the memory file
+
+    mov r14, [rsp] ; argc
+
+    mov rax, SYS_EXECVEAT
+    mov rdi, r13
+    mov rsi, empty_str
+    lea rdx, [rsp + 8] ; argv
+    lea r10, [rsp + 8 + (r14 * 8) + 8] ; envp
+    mov r8, AT_EMPTY_PATH ; AT_EMPTY_PATH
+    syscall
+
+_exit:
+    mov rax, SYS_EXIT
+    mov rdi, 1
+    syscall
